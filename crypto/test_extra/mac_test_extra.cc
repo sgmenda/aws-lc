d03c76503206d9c03ba1f7e20a35051635b85683
Sanketh Menda <sgmenda@amazon.com>
2025-10-13 16:14:35 -0700

    refactor to simplify mac tests

--- crypto/test_extra/mac_test_extra.cc
+++ crypto/test_extra/mac_test_extra.cc
@@ -16,15 +16,16 @@ class RooterbergMac {
   virtual bool OneShotMAC(std::vector<uint8_t> &macd,
                           const std::vector<uint8_t> &key,
                           const std::vector<uint8_t> &msg) const = 0;
+  explicit RooterbergMac(std::string name) { test_name = name; };
 };
 
 /** tests HMAC with given digest */
 class RooterbergHmac : public RooterbergMac {
  public:
-  std::string test_name;
   const EVP_MD *(*digest)(void);
+
   RooterbergHmac(std::string name, const EVP_MD *(*md)(void))
-      : test_name(name), digest(md) {};
+      : RooterbergMac(name), digest(md) {};
   unsigned long MacSize() const override {
     return (8 * EVP_MD_size(digest()));  // HMAC size = digest size
   }
@@ -41,10 +42,10 @@ class RooterbergHmac : public RooterbergMac {
 /** tests HMAC with given digest */
 class RooterbergCmac : public RooterbergMac {
  public:
-  std::string test_name;
   const EVP_CIPHER *(*cipher)(void);
+
   RooterbergCmac(std::string name, const EVP_CIPHER *(*cp)(void))
-      : test_name(name), cipher(cp) {};
+      : RooterbergMac(name), cipher(cp) {};
   unsigned long MacSize() const override {
     return (8 * 16);  // Always 16 bytes
   }
@@ -58,6 +59,35 @@ class RooterbergCmac : public RooterbergMac {
   }
 };
 
+static void RunRooterbergMacTest(const RooterbergMac *param) {
+  // 0. Parse JSON file
+  std::ifstream f(MAC_TEST_VECTOR_PATH + param->test_name + ".json");
+  ASSERT_TRUE(f.is_open()) << "failed to open test file";
+  json data = json::parse(f);
+
+  // 1. Validate algorithm params
+  ASSERT_EQ(data["testType"], "Mac");
+  ASSERT_EQ(data["algorithm"]["algorithmType"], "Mac");
+  ASSERT_EQ(data["algorithm"]["macSize"].get<size_t>(),
+            param->MacSize());  // HMAC size = digest size
+
+  // 2. Enumerate tests
+  for (auto test_case : data["tests"]) {
+    std::vector<uint8_t> key = JsonHexToBytes(test_case["key"]);
+    std::vector<uint8_t> msg = JsonHexToBytes(test_case["msg"]);
+    std::vector<uint8_t> mac = JsonHexToBytes(test_case["mac"]);
+
+    std::vector<uint8_t> macd(mac.size());
+
+    // Test one-shot API
+    ASSERT_TRUE(param->OneShotMAC(macd, key, msg));
+    EXPECT_EQ(Bytes(mac), Bytes(macd));
+
+    // TODO: Test incremental API?
+  }
+}
+
+
 static RooterbergHmac rHMACs[] = {
     {"hmac_sha1_160", EVP_sha1},
     {"hmac_sha256_256", EVP_sha256},
@@ -70,50 +100,20 @@ static RooterbergHmac rHMACs[] = {
     {"hmac_sha3_512_512", EVP_sha3_512},
 };
 
-static const RooterbergCmac rCMACs[] = {
-    {"aes_cmac_128_128", EVP_aes_128_cbc},
-    // {"aes_cmac_192_128", EVP_aes_192_cbc},
-    {"aes_cmac_256_128", EVP_aes_256_cbc},
-};
-
-// FIXME: HmacTest and CmacTest are almost identical, refactor to avoid
-// copy-paste.
-
 class RooterbergHmacTest : public testing::TestWithParam<RooterbergHmac> {};
 INSTANTIATE_TEST_SUITE_P(
     All, RooterbergHmacTest, testing::ValuesIn(rHMACs),
     [](const testing::TestParamInfo<RooterbergHmac> &params) -> std::string {
       return params.param.test_name;
     });
+TEST_P(RooterbergHmacTest, TestName) { RunRooterbergMacTest(&GetParam()); };
 
-TEST_P(RooterbergHmacTest, TestName) {
-  // 0. Parse JSON file
-  std::ifstream f(MAC_TEST_VECTOR_PATH + GetParam().test_name + ".json");
-  ASSERT_TRUE(f.is_open()) << "failed to open test file";
-  json data = json::parse(f);
 
-  // 1. Validate algorithm params
-  ASSERT_EQ(data["testType"], "Mac");
-  ASSERT_EQ(data["algorithm"]["algorithmType"], "Mac");
-  ASSERT_EQ(data["algorithm"]["macSize"].get<size_t>(),
-            GetParam().MacSize());  // HMAC size = digest size
-
-  // 2. Enumerate tests
-  for (auto test_case : data["tests"]) {
-    std::vector<uint8_t> key = JsonHexToBytes(test_case["key"]);
-    std::vector<uint8_t> msg = JsonHexToBytes(test_case["msg"]);
-    std::vector<uint8_t> mac = JsonHexToBytes(test_case["mac"]);
-
-    std::vector<uint8_t> macd(mac.size());
-
-    // Test one-shot API
-    ASSERT_TRUE(GetParam().OneShotMAC(macd, key, msg));
-    EXPECT_EQ(Bytes(mac), Bytes(macd));
-
-    // TODO: Test incremental API?
-  }
+static const RooterbergCmac rCMACs[] = {
+    {"aes_cmac_128_128", EVP_aes_128_cbc},
+    // {"aes_cmac_192_128", EVP_aes_192_cbc},
+    {"aes_cmac_256_128", EVP_aes_256_cbc},
 };
-
 class RooterbergCmacTest : public testing::TestWithParam<RooterbergCmac> {};
 INSTANTIATE_TEST_SUITE_P(
     All, RooterbergCmacTest, testing::ValuesIn(rCMACs),
@@ -121,30 +121,4 @@ INSTANTIATE_TEST_SUITE_P(
       return params.param.test_name;
     });
 
-TEST_P(RooterbergCmacTest, TestName) {
-  // 0. Parse JSON file
-  std::ifstream f(MAC_TEST_VECTOR_PATH + GetParam().test_name + ".json");
-  ASSERT_TRUE(f.is_open()) << "failed to open test file";
-  json data = json::parse(f);
-
-  // 1. Validate algorithm params
-  ASSERT_EQ(data["testType"], "Mac");
-  ASSERT_EQ(data["algorithm"]["algorithmType"], "Mac");
-  ASSERT_EQ(data["algorithm"]["macSize"].get<size_t>(),
-            GetParam().MacSize());  // HMAC size = digest size
-
-  // 2. Enumerate tests
-  for (auto test_case : data["tests"]) {
-    std::vector<uint8_t> key = JsonHexToBytes(test_case["key"]);
-    std::vector<uint8_t> msg = JsonHexToBytes(test_case["msg"]);
-    std::vector<uint8_t> mac = JsonHexToBytes(test_case["mac"]);
-
-    std::vector<uint8_t> macd(mac.size());
-
-    // Test one-shot API
-    ASSERT_TRUE(GetParam().OneShotMAC(macd, key, msg));
-    EXPECT_EQ(Bytes(mac), Bytes(macd));
-
-    // TODO: Test incremental API?
-  }
-};
+TEST_P(RooterbergCmacTest, TestName) { RunRooterbergMacTest(&GetParam()); };
